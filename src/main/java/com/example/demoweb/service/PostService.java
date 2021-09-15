b7e64eedc3f5a45bcc613dc896d9f1f5b7600379
Vlad <vladpiligrimov@gmail.com>
2021-09-15 19:40:59 +0500

    added controller

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -5,15 +5,26 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class PostService {
+    private final ArrayList<Post> _posts;
+
+    public PostService() {
+        this._posts = new ArrayList<>();
+        _posts.add(new Post("Post #1", new Date(), 55));
+        _posts.add(new Post("Post #2", new Date(), 68));
+        _posts.add(new Post("Post #3", new Date(), 130));
+    }
+
     public Post[] listAllPosts() {
-        return new Post[]{
-                new Post("Post #1", new Date(), 55),
-                new Post("Post #2", new Date(), 68),
-                new Post("Post #3", new Date(), 130),
-        };
+        return _posts.toArray(new Post[0]);
+    }
+
+    public void create(String text) {
+        _posts.add(new Post(text, new Date(), 0));
     }
 }
